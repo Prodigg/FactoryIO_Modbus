8e770727398f9a3c22379e68aa057bb6be7fe913
prodigg <raphael@familie-sauer.ch>
2025-10-07 20:35:08 +0200

    Projektdateien hinzufügen.

---
+++ FactoryIOLib/FactoryIO.h
@@ -0,0 +1,3 @@
+/*!
+The main Factory IO lib include
+*/
---
+++ FactoryIOLibTest/FactoryIOLibTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FactoryIOLibTest
+{
+	TEST_CLASS(FactoryIOLibTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
